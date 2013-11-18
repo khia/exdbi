c75f5358ab6db8506cc659b716e5f9593efabc74
ilya khlopotov <ilya@spawngrid.com>
2013-11-18 13:39:55 -0800

    Extend zip API to support extraction of single column and filtering out not needed columns

--- lib/result.ex
+++ lib/result.ex
@@ -4,6 +4,19 @@ defrecord DBI.Result, count: nil, columns: [], rows: [] do
                                              tuple_size(row) == length(columns) do
     Enum.zip(columns, tuple_to_list(row))
   end
+  def zip(column, __MODULE__[columns: columns, rows: rows]) when is_binary(column) do
+    idx = Enum.find_index(columns, &(&1 == column))
+    lc row inlist rows, do: elem(row, idx)
+  end
+
+  def zip(requested_columns, __MODULE__[columns: columns, rows: rows]) when is_list(requested_columns) do
+    indexes = Enum.filter_map(Enum.with_index(columns),
+      fn({column, _idx}) -> column in requested_columns end,
+      fn(ci) -> ci end)
+    lc row inlist rows do
+      lc {column, idx} inlist indexes, do: {column, elem(row, idx)}
+    end
+  end
 
   def zip(__MODULE__[columns: columns, rows: rows]) do
     lc row inlist rows, do: Enum.zip(columns, tuple_to_list(row))
